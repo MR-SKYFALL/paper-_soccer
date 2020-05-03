57acdcbdaf0aabdf0baa8f683a2ac27f807d6ca0
Dawid Matras <46224840+MR-SKYFALL@users.noreply.github.com>
2020-05-03 12:06:16 +0200

    Update connect.php

--- php_scripts/utilities_php/connect.php
+++ php_scripts/utilities_php/connect.php
@@ -1,5 +1,5 @@
 <?php
-$host = "23181.m.tld.pl";
-$db_user = "admin23181_paper_soocer";
-$db_password = "5WfBUg-mUq";
-$db_name = "baza23181_paper_soocer";
+$host = "";
+$db_user = "";
+$db_password = "";
+$db_name = "";
